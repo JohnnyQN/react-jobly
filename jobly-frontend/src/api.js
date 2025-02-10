8038a98b4a741e5231620e2fa3358b5b44bccafc
Johnny Ngo <johnny.q.ngo@gmail.com>
2025-02-09 23:55:44 -0800

    Updated API URL for Render backend

--- jobly-frontend/src/api.js
+++ jobly-frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3001";
+const BASE_URL = process.env.REACT_APP_BASE_URL || "https://react-jobly-backend-4vak.onrender.com";
 
 /** API Class.
  *
